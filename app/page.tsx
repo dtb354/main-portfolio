5fbac66dfd643165c4464ad34da2dadb04d07e07
Daniel Tran <danieltran354@gmail.com>
2026-01-07 22:54:57 +0100

    Messed around with routing and installed stuff lol

--- app/page.tsx
+++ app/page.tsx
@@ -15,14 +15,6 @@ export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
         <div style={{ height: '200px', position: 'relative', overflow: 'hidden'}}>
           {/* Basic horizontal loop */}
           <LogoLoop
@@ -32,6 +24,7 @@ export default function Home() {
               logoHeight={48}
               gap={40}
               hoverSpeed={0}
+              width="36rem"
               scaleOnHover
               ariaLabel="Technology partners"
           />
